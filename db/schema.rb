5e63932fb6b7105e23d5c5c39558bdc2756ece57
giran-git <giran904587034+git@gmail.com>
2021-07-15 07:27:19 +0000

    マイグレーション修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_063805) do
+ActiveRecord::Schema.define(version: 2021_07_11_060242) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
