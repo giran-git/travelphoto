29da8bb4aa7523b631578032d64593d2c63da84c
giran-git <giran904587034+git@gmail.com>
2021-07-25 11:11:20 +0000

    フォロー修正

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,41 +1,41 @@
 class Public::RelationshipsController < ApplicationController
-   before_action :authenticate_user!
+  # before_action :authenticate_user!
 
-  def create
-    following = current_customer.follow(@user)
-    if following.save
-      flash[:success] = 'ユーザーをフォローしました'
-      redirect_to @customer
-    else
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @customer
-    end
-  end
+  # def create
+  #   following = current_customer.follow(@user)
+  #   if following.save
+  #     flash[:success] = 'ユーザーをフォローしました'
+  #     redirect_to @customer
+  #   else
+  #     flash.now[:alert] = 'ユーザーのフォローに失敗しました'
+  #     redirect_to @customer
+  #   end
+  # end
 
-  def destroy
-    following = current_customer.unfollow(@customer)
-    if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to @customer
-    else
-      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @customer
-    end
-  end
+  # def destroy
+  #   following = current_customer.unfollow(@customer)
+  #   if following.destroy
+  #     flash[:success] = 'ユーザーのフォローを解除しました'
+  #     redirect_to @customer
+  #   else
+  #     flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
+  #     redirect_to @customer
+  #   end
+  # end
 
-  private
-  def
-    @customer = Customer.find(params[:relationship][:follow_id])
-  end
+  # private
+  # def
+  #   @customer = Customer.find(params[:relationship][:follow_id])
+  # end
 
-  #————————フォロー・フォロワー一覧を表示する-————————————
-  def followings
-    customer = Customer.find(params[:customer_id])
-    @customers = customer.followings
-  end
+  # #————————フォロー・フォロワー一覧を表示する-————————————
+  # def followings
+  #   customer = Customer.find(params[:customer_id])
+  #   @customers = customer.followings
+  # end
 
-  def followers
-    customer = Customer.find(params[:customer_id])
-    @customers = customer.followers
-  end
+  # def followers
+  #   customer = Customer.find(params[:customer_id])
+  #   @customers = customer.followers
+  # end
 end
