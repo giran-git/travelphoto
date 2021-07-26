4893caa1229d43062e15368aad42a1d19612b445
giran-git <giran904587034+git@gmail.com>
2021-07-26 12:34:47 +0000

    tt

--- db/schema.rb
+++ db/schema.rb
@@ -26,12 +26,12 @@ ActiveRecord::Schema.define(version: 2021_07_26_091354) do
 
   create_table "comments", force: :cascade do |t|
     t.string "comment_content"
-    t.integer "user_id"
+    t.integer "customer_id"
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_comments_on_customer_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "customers", force: :cascade do |t|
