072aedf5b20c9735ba76a9c22f9b35c7dd98925c
giran-git <giran904587034+git@gmail.com>
2021-08-11 10:53:16 +0000

    レイアウト修正

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,6 @@
 class Public::PostsController < ApplicationController
 before_action :authenticate_customer!
-  # before_action :ensure_correct_customer, only: [:edit, :update, :destroy]
+before_action :ensure_correct_customer, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -17,7 +17,9 @@ before_action :authenticate_customer!
     #コメント機能
     @comments = @post.comments
     @comment = current_customer.comments.new
+  end
 
+  def edit
   end
 
   def create
@@ -32,14 +34,29 @@ before_action :authenticate_customer!
       render :index
     end
   end
+
+  def update
+      if @post.update(post_params)
+        redirect_to request.referer
+      else
+        render :edit
+      end
+  end
   def destroy
-      @post = Post.find_by(id: params[:id])
       @post.destroy
       redirect_to request.referer
   end
 
-  private
- def post_params
-  params.require(:post).permit(:post_content, :title, :body, :image, :name, :post_area, :text, :genre_id, :location_id)
- end
+private
+
+  def post_params
+    params.require(:post).permit(:post_content, :title, :body, :image, :name, :post_area, :text, :genre_id, :location_id)
+  end
+
+  def ensure_correct_customer
+    @post = Post.find(params[:id])
+    unless @post.customer == current_customer
+      redirect_to posts_path
+    end
+  end
 end
